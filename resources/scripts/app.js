99157a5fa3cee5f906f2287839ea4f0bd8a4617c
Juri Leino <github@line-o.de>
2023-06-28 11:43:17 +0000

    feat(letter): add language color toggle
    Toggles highlighting different languages used in currently viewed
    transcription of a letter.

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,8 +1,29 @@
 window.addEventListener('DOMContentLoaded', function() {
-    
+    const toggle = document.querySelector('[name=lang-toggle]')
+
+    function applyLangColors () {
+        const view = document.getElementById('view1')
+        if (toggle.checked) {
+            // console.log('lang-toggle', 'checked')
+            view.style.setProperty("--lang-de-color", 'var(--bb-lang-de-color)');
+            view.style.setProperty("--lang-el-color", 'var(--bb-lang-el-color)');
+            view.style.setProperty("--lang-la-color", 'var(--bb-lang-la-color)');
+        } else {
+            // console.log('lang-toggle', 'unchecked')
+            view.style.setProperty("--lang-la-color", 'transparent');
+            view.style.setProperty("--lang-el-color", 'transparent');
+            view.style.setProperty("--lang-de-color", 'transparent');
+        }
+    }
+
+    toggle.addEventListener('change', applyLangColors)
+
     pbEvents.subscribe('pb-update', 'transcription', ev => {
         const shadowroot = ev.detail.root;
-        if(!(ev.target.id === "facsimile-links")) return;         
+        // set initial value
+        applyLangColors()
+
+        if(!(ev.target.id === "facsimile-links")) return;
         const pblinks = document.getElementById("facsimile-links")
         const empty = pblinks.shadowRoot.querySelector('.content');
         if(empty.textContent=== "") {
